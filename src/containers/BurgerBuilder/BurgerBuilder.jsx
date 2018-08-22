b094ffe69c3cc6ebc15ceea575d4fd5787faf196
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-08-22 16:01:06 +0600

    Add static burger

---
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/aux'
+import Burger from './../../components/Burger/Burger';
+ class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>
+          <Burger></Burger>
+        </div>
+        <div>Build Controls</div>
+      </Aux>
+    )
+  }
+} 
+
+export default BurgerBuilder;
